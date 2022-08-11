717d33ded3c0f7a7000ab6623531bddf377cab4f
Earl Harris <earleharrisjr@gmail.com>
2022-08-11 12:51:12 -0400

    add delete a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -70,6 +70,16 @@ export class HeroService {
     );
   }
 
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
   /** Log a HeroService message with the MessageService */
